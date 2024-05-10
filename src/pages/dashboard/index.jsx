9ef041b5e9b4b81b373e5b53fae2de7629c691e7
Heloisa Tavares <heloisamt@gmail.com>
2024-05-10 17:41:47 -0300

    Inclusão da Tabela de Localidades cadastradas, reestilização dos cards para mais espaço para tabela

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,16 +2,27 @@ import { Box, Card, CardContent, Typography } from "@mui/material";
 
 import styles from "./style.module.css";
 import { MapPin, Users } from "lucide-react";
+import ListDash from "../../components/listDash";
 
 export function Dashboard() {
   return (
     <div className={styles.mainContainer}>
-      <Typography>Dashboard</Typography>
-      <h3>Cards</h3>
       <div className={styles.cardContainerLine}>
-        
-        <div className={styles.cardContainer} >
-          <Card sx={{ height: 150, width: 300, borderRadius: 3, backgroundColor:'#0F0F0F' }}>
+        <div className={styles.containerText}>
+          <Typography fontSize={32} fontWeight={500} fontStyle={"oblique"}>
+            Dashboard
+          </Typography>
+          <Typography fontSize={20}>Total cadastrado:</Typography>
+        </div>
+        <div className={styles.cardContainer}>
+          <Card
+            sx={{
+              height: 130,
+              width: 250,
+              borderRadius: 3,
+              backgroundColor: "#0F0F0F",
+            }}
+          >
             <CardContent
               sx={{
                 display: "flex",
@@ -20,28 +31,35 @@ export function Dashboard() {
                 alignItems: "center",
               }}
             >
-              <Typography fontSize={20} color={"#F4F6F4"}>
+              <Typography fontSize={16} color={"#F4F6F4"}>
                 Usuários
               </Typography>
               <div className={styles.cardContentContainer}>
                 <Box>
                   <Typography
                     className={styles.number}
-                    fontSize={40}
+                    fontSize={36}
                     fontWeight={600}
                   >
                     25
                   </Typography>
                 </Box>
                 <Box>
-                  <Users size={50} className={styles.icon} />
+                  <Users size={40} className={styles.icon} />
                 </Box>
               </div>
             </CardContent>
           </Card>
         </div>
-        <div className={styles.cardContainer} >
-          <Card sx={{ height: 150, width: 300, borderRadius: 3, backgroundColor:'#0F0F0F' }}>
+        <div className={styles.cardContainer}>
+          <Card
+            sx={{
+              height: 130,
+              width: 250,
+              borderRadius: 3,
+              backgroundColor: "#0F0F0F",
+            }}
+          >
             <CardContent
               sx={{
                 display: "flex",
@@ -50,33 +68,35 @@ export function Dashboard() {
                 alignItems: "center",
               }}
             >
-              <Typography fontSize={20} color={"#F4F6F4"}>
+              <Typography fontSize={16} color={"#F4F6F4"}>
                 Locais
               </Typography>
               <div className={styles.cardContentContainer}>
                 <Box>
                   <Typography
                     className={styles.number}
-                    fontSize={40}
+                    fontSize={36}
                     fontWeight={600}
                   >
                     25
                   </Typography>
                 </Box>
                 <Box>
-                  <MapPin  size={50} className={styles.icon} />
+                  <MapPin size={40} className={styles.icon} />
                 </Box>
               </div>
             </CardContent>
           </Card>
         </div>
       </div>
-      <div>
-        <Typography>Locais</Typography>
-        <Typography>Lista dos locais cadastrados</Typography>
+      <div className={styles.listContainerText} >
+        <Typography fontSize={32} fontWeight={500} fontStyle={"oblique"}>
+          Locais
+        </Typography>
+        <Typography fontSize={18}>Lista dos locais cadastrados</Typography>
       </div>
       <div className={styles.listContainer}>
-        <h3>Lista</h3>
+        <ListDash></ListDash>
       </div>
     </div>
   );
