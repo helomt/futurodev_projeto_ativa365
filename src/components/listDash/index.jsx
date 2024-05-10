2bbee399200324f89af245bf60ca50ceeb4ba1e6
Heloisa Tavares <heloisamt@gmail.com>
2024-05-10 17:40:41 -0300

    Criação da Lista de locais da Dashboard, estilização da header e simulação nas células para os dados

---
+++ src/components/listDash/index.jsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+import { styled } from "@mui/material";
+const columns = [
+  { id: "local", label: "Localidade", width: 170},
+  {
+    id: "usuario",
+    label: "Usuário",
+    minWidth: 170,
+    align: "left",
+  },
+];
+
+function createData(local, usuario) {
+  return { local, usuario };
+}
+
+const rows = [
+  createData("Jurere ", "Gustavo"),
+  createData("Academia Bio Ritmo", "Heloisa"),
+  createData("Jurere ", "Gustavo"),
+  createData("Academia Bio Ritmo", "Heloisa"),
+  createData("Jurere ", "Gustavo"),
+  createData("Academia Bio Ritmo", "Heloisa"),
+  createData("Jurere ", "Gustavo"),
+  createData("Academia Bio Ritmo", "Heloisa"),
+];
+
+export default function ListDash() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const StyledTableCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#0F0F0F",
+      color: '#F4F6F4',
+      fontSize: 16,
+    },
+  }));
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <StyledTableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </StyledTableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
