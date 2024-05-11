103c77b4415760330c568b48cd1b819945b3dc9e
Heloisa Tavares <heloisamt@gmail.com>
2024-05-11 18:49:35 -0300

    Criação do botão para adicionar novo cadastro de local e cabeçalho da página

--- src/pages/locais/index.jsx
+++ src/pages/locais/index.jsx
@@ -1,5 +1,41 @@
-import styles from './style.module.css'
+import { Button, Typography } from "@mui/material";
+import styles from "./style.module.css";
+import ListPlaces from "../../components/listPlaces";
 
-export function Locais(){
-    return (<di><h1 className={styles.container}>Locais</h1></di>)
+
+
+export function Locais() {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.containerHeader}>
+        <div>
+          <Typography fontSize={40} fontWeight={500} fontStyle={"oblique"}>
+            Locais
+          </Typography>
+          <Typography>
+            Localidades de atividade esportivas cadastradas
+          </Typography>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            sx={{
+              width: 300,
+              height: 50,
+              bgcolor: "#FF0A1B",
+              "&:hover": { backgroundColor: "#F35359" },
+            }}
+          >
+            Cadastrar
+          </Button>
+          
+        </div>
+      </div>
+      <div className={styles.table}>
+        <ListPlaces></ListPlaces>
+    
+        
+      </div>
+    </div>
+  );
 }
