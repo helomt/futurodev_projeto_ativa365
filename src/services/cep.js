162121848c8238d4a3f810f9536b1660d6ff34c7
Heloisa Tavares <heloisamt@gmail.com>
2024-05-12 20:34:37 -0300

    Alteração do nome da pasta service

---
+++ src/services/cep.js
@@ -0,0 +1,8 @@
+export async function buscarEndereco(cep) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    if (data.erro) {
+      throw new Error('CEP não encontrado');
+    }
+    return data;
+}
