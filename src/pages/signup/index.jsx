3c2c5531a2b829b564c235cd0d11a6d58cf7947b
Heloisa Tavares <heloisamt@gmail.com>
2024-05-12 11:02:11 -0300

    Inclusão do Public Template, e do link para página de cadastro

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -2,46 +2,50 @@ import { TextField, Typography } from "@mui/material";
 import styles from "./style.module.css";
 import { InputForm } from "../../components/inputForm/inputForm";
 import { ButtonForm } from "../../components/buttonForm/buttonForm";
+import { PublicTemplate } from "../../template/public";
+import { Link } from "react-router-dom";
 
 export function SignUp() {
   return (
-    <div className={styles.container}>
-      <Typography fontSize={35} fontWeight={500} color={"#FF0A1B"}>
-        Cadastre-se
-      </Typography>
-      <form className={styles.form} autoComplete="off">
-        <div className={styles.row}>
-          <InputForm labelText={"Nome"} required={true} type="text" />
+    <PublicTemplate>
+      <div className={styles.container}>
+        <Typography fontSize={35} fontWeight={500} color={"#FF0A1B"}>
+          Cadastre-se
+        </Typography>
+        <form className={styles.form} autoComplete="off">
+          <div className={styles.row}>
+            <InputForm labelText={"Nome"} required={true} type="text" />
 
-          <InputForm
-            labelText={"Data de Nascimento"}
-            required={true}
-            type={"text"}
-          />
-        </div>
-        <div className={styles.row}>
-          <InputForm labelText={"CPF"} required={true} type="number" />
-          <InputForm labelText={"Email"} required={true} type="email" />
-        </div>
-        <div className={styles.row}>
-          <InputForm labelText={"Senha"} required={true} type="text" />
-          <InputForm labelText={"CEP"} required={true} type="number" />
-        </div>
-        <div className={styles.row}>
-          <InputForm labelText={"Endereço"} required={false} type="text" />
-          <InputForm labelText={"Número"} required={true} type="number" />
-        </div>
-        <div>
-          <TextField
-            sx={{ width: 505 }}
-            label="Complemento"
-            color="error"
-            type="text"
-          />
-        </div>
-        <ButtonForm>Cadastrar</ButtonForm>
-      </form>
-      <Typography>Já possui conta? Faça seu login.</Typography>
-    </div>
+            <InputForm
+              labelText={"Data de Nascimento"}
+              required={true}
+              type={"text"}
+            />
+          </div>
+          <div className={styles.row}>
+            <InputForm labelText={"CPF"} required={true} type="number" />
+            <InputForm labelText={"Email"} required={true} type="email" />
+          </div>
+          <div className={styles.row}>
+            <InputForm labelText={"Senha"} required={true} type="text" />
+            <InputForm labelText={"CEP"} required={true} type="number" />
+          </div>
+          <div className={styles.row}>
+            <InputForm labelText={"Endereço"} required={false} type="text" />
+            <InputForm labelText={"Número"} required={true} type="number" />
+          </div>
+          <div>
+            <TextField
+              sx={{ width: 505 }}
+              label="Complemento"
+              color="error"
+              type="text"
+            />
+          </div>
+          <ButtonForm>Cadastrar</ButtonForm>
+        </form>
+        <Link to={'/'}><Typography>Já possui conta? Faça seu login.</Typography></Link> 
+      </div>
+    </PublicTemplate>
   );
 }
