914b32b632f5fa79fe81d05ef998e5541b0a439d
Heloisa Tavares <heloisamt@gmail.com>
2024-05-12 17:50:43 -0300

    Alteção no componente de input, criação da função handlesubmit e register nas entradas

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,38 +1,108 @@
 import { TextField, Typography } from "@mui/material";
 import styles from "./style.module.css";
-import { InputForm } from "../../components/inputForm/inputForm";
 import { ButtonForm } from "../../components/buttonForm/buttonForm";
 import { PublicTemplate } from "../../template/public";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export function SignUp() {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <PublicTemplate>
       <div className={styles.container}>
         <Typography fontSize={35} fontWeight={500} color={"#FF0A1B"}>
           Cadastre-se
         </Typography>
-        <form className={styles.form} autoComplete="off">
+        <form
+          className={styles.form}
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className={styles.row}>
-            <InputForm labelText={"Nome"} required={true} type="text" />
-
-            <InputForm
-              labelText={"Data de Nascimento"}
-              required={true}
-              type={"text"}
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"Nome"}
+              required
+              type="text"
+              sx={{ width: 250 }}
+              {...register("nome")}
+            />
+            <TextField
+              color="error"
+              InputLabelProps={{ shrink: true }}
+              variant="outlined"
+              label={"Data de Nascimento"}
+              required
+              type="date"
+              sx={{ width: 250 }}
+              {...register("data")}
             />
           </div>
           <div className={styles.row}>
-            <InputForm labelText={"CPF"} required={true} type="number" />
-            <InputForm labelText={"Email"} required={true} type="email" />
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"CPF"}
+              required
+              type="text"
+              sx={{ width: 250 }}
+              {...register("cpf")}
+            />
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"Email"}
+              required
+              type="email"
+              sx={{ width: 250 }}
+              {...register("email")}
+            />
           </div>
           <div className={styles.row}>
-            <InputForm labelText={"Senha"} required={true} type="text" />
-            <InputForm labelText={"CEP"} required={true} type="number" />
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"Senha"}
+              required
+              type="text"
+              sx={{ width: 250 }}
+              {...register("senha")}
+            />
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"CEP"}
+              required
+              type="text"
+              sx={{ width: 250 }}
+              {...register("cep")}
+            />
           </div>
           <div className={styles.row}>
-            <InputForm labelText={"Endereço"} required={false} type="text" />
-            <InputForm labelText={"Número"} required={true} type="number" />
+         
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"Endereço"}
+              type="text"
+              sx={{ width: 250 }}
+              {...register("endereco")}
+            />
+            <TextField
+              color="error"
+              variant="outlined"
+              label={"Número"}
+              required
+              type="number"
+              sx={{ width: 250 }}
+              {...register("numero")}
+            />
           </div>
           <div>
             <TextField
@@ -40,11 +110,14 @@ export function SignUp() {
               label="Complemento"
               color="error"
               type="text"
+              {...register("complemento")}
             />
           </div>
-          <ButtonForm>Cadastrar</ButtonForm>
+          <ButtonForm type="submit">Cadastrar</ButtonForm>
         </form>
-        <Link to={'/'}><Typography>Já possui conta? Faça seu login.</Typography></Link> 
+        <Link to={"/"}>
+          <Typography>Já possui conta? Faça seu login.</Typography>
+        </Link>
       </div>
     </PublicTemplate>
   );
