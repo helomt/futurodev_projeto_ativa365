a99ad62eaad90e726e652243243f739739dd8869
Heloisa Tavares <heloisamt@gmail.com>
2024-05-12 11:03:07 -0300

    Inclusão do Public Template, e do link para a página de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,37 +2,41 @@ import { TextField, Typography } from "@mui/material";
 import { ButtonFormBlack } from "../../components/buttonFormBlack/buttonFormBlack";
 import { ButtonForm } from "../../components/buttonForm/buttonForm";
 
-
 import styles from "./style.module.css";
-
+import { PublicTemplate } from "../../template/public";
+import { Link } from "react-router-dom";
 
 export function Login() {
   return (
-    <div>
-      <div className={styles.container}>
-        <Typography fontSize={45} fontWeight={500} color={"#FF0A1B"}>
-          Efetue seu login
-        </Typography>
-        <Typography fontStyle={"italic"} fontSize={20}>
-          Comece agora a sua jornada{" "}
-        </Typography>
-        <form className={styles.form}>
-          <TextField
-            type="email"
-            label="Email"
-            variant="outlined"
-            color="error"
-          />
-          <TextField
-            type="password"
-            label="Senha"
-            variant="outlined"
-            color="error"
-          />
-          <ButtonForm>Entrar</ButtonForm>
-        </form>
-        <ButtonFormBlack>Cadastre-se</ButtonFormBlack>
+    <PublicTemplate>
+      <div>
+        <div className={styles.container}>
+          <Typography fontSize={45} fontWeight={500} color={"#FF0A1B"}>
+            Efetue seu login
+          </Typography>
+          <Typography fontStyle={"italic"} fontSize={20}>
+            Comece agora a sua jornada{" "}
+          </Typography>
+          <form className={styles.form}>
+            <TextField
+              type="email"
+              label="Email"
+              variant="outlined"
+              color="error"
+            />
+            <TextField
+              type="password"
+              label="Senha"
+              variant="outlined"
+              color="error"
+            />
+            <ButtonForm>Entrar</ButtonForm>
+          </form>
+          <Link to={"/cadastro"}>
+            <ButtonFormBlack>Cadastre-se</ButtonFormBlack>
+          </Link>
+        </div>
       </div>
-    </div>
+    </PublicTemplate>
   );
 }
