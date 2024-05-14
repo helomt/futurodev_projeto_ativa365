fbe92767e530b9c06ee259fb547ff3b8ba2f9d72
Heloisa Tavares <heloisamt@gmail.com>
2024-05-14 15:30:43 -0300

    Criação de funções de adição, edição, remoção e busca de 1 ou mais locais assim como funções de contar e buscar um ultimo id para automatização do id

---
+++ src/services/serverLocais.js
@@ -0,0 +1,112 @@
+// GET
+export async function buscarLocais() {
+  const resposta = await fetch("http://localhost:3000/locais");
+  const data = await resposta.json();
+  if (data.erro) {
+    throw new alert("Locais não encontrado");
+  }
+  return data;
+}
+
+//GET 1
+export async function buscarUmLocal(id) {
+  const resposta = await fetch(`http://localhost:3000/locais/${id}`, {
+    method: "GET",
+  });
+  const data = await resposta.json();
+  if (data.erro) {
+    throw new alert("Local não encontrado");
+  }
+  return data;
+}
+
+// DELETE
+export async function deletarLocal(id) {
+  const response = await fetch(`http://localhost:3000/locais/${id}`, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  if (data.erro) {
+    throw new alert("Local não deletado");
+  }
+  return data;
+}
+
+//POST
+export async function criarLocal(values, userId) {
+  const id = Number(await buscarUltimoLocal()) + 1;
+  const local = {
+    id: String(id),
+    nome: values.nome,
+    atividade: values.atividade,
+    descricao: values.descricao,
+    endereco: values.endereco,
+    cep: values.cep,
+    complemento: values.complemento,
+    localidade: values.localidade,
+    numero: values.numero,
+    userId: userId,
+  };
+  const response = await fetch("http://localhost:3000/locais", {
+    method: "POST",
+    body: JSON.stringify(local),
+  });
+  const data = await response.json();
+  if (data.erro) {
+    throw new alert("Local não adicionado");
+  }
+  return data;
+}
+
+//PUT
+export async function atualizarLocal(id, values, userId) {
+  const local = {
+    id: id,
+    nome: values.nome,
+    atividade: values.atividade,
+    descricao: values.descricao,
+    endereco: values.endereco,
+    cep: values.cep,
+    complemento: values.complemento,
+    localidade: values.localidade,
+    numero: values.numero,
+    userId: userId,
+  };
+  const response = await fetch(`http://localhost:3000/locais/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(local),
+  });
+  const data = await response.json();
+  if (data.erro) {
+    throw new alert("Local não atualizado");
+  }
+  return data;
+}
+
+export async function contarLocais() {
+  const lista = await buscarLocais();
+  const qtd = lista.length;
+  return qtd;
+}
+
+export async function buscarUltimoLocal() {
+  const lista = await buscarLocais();
+  const id = lista.at(-1).id;
+  return id;
+}
+
+// var local = {
+//   nome: "Plenum",
+//   atividade: "Escalada",
+//   descricao: "",
+//   endereco: "Rua José",
+//   cep: "88705060",
+//   complemento: "",
+//   localidade: "Tubarão",
+//   numero: 25,
+// }
+
+// var userId = "2"
+// console.log(await criarLocal(local,userId))
+
+// // console.log(await buscarUmLocal("1"));
