16686bc0383ef7e22781e88293d1906282de7077
Heloisa Tavares <heloisamt@gmail.com>
2024-05-14 22:30:08 -0300

    Alteração na resposta em caso de erro

--- src/services/cep.js
+++ src/services/cep.js
@@ -2,7 +2,7 @@ export async function buscarEndereco(cep) {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await response.json();
     if (data.erro) {
-      throw new Error('CEP não encontrado');
+      throw new alert('CEP não encontrado');
     }
     return data;
 }
