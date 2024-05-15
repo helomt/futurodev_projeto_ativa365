1b390602bed00d6a9ea8625299360d899e9b12a1
Heloisa Tavares <heloisamt@gmail.com>
2024-05-14 22:39:46 -0300

    Alterações devido a tentativas diferentes de importação porém sem nenhuma mudança significativa

--- src/pages/locais/index.jsx
+++ src/pages/locais/index.jsx
@@ -1,9 +1,10 @@
-import { Button, Typography } from "@mui/material";
+import {
+  Button,
+  Typography,
+} from "@mui/material";
 import styles from "./style.module.css";
-import { ListPlaces } from "../../components/listPlaces";
 import { Link } from "react-router-dom";
-
-
+import { ListPlaces } from "../../components/listPlaces";
 
 export function Locais() {
   return (
@@ -18,26 +19,23 @@ export function Locais() {
           </Typography>
         </div>
         <div>
-          <Link to={'/dashboard/locais/registro'}>
-          <Button
-            variant="contained"
-            sx={{
-              width: 300,
-              height: 50,
-              bgcolor: "#FF0A1B",
-              "&:hover": { backgroundColor: "#F35359" },
-            }}
-          >
-            Cadastrar
-          </Button>
+          <Link to={"/dashboard/locais/registro"}>
+            <Button
+              variant="contained"
+              sx={{
+                width: 300,
+                height: 50,
+                bgcolor: "#FF0A1B",
+                "&:hover": { backgroundColor: "#F35359" },
+              }}
+            >
+              Cadastrar
+            </Button>
           </Link>
-          
         </div>
       </div>
       <div className={styles.table}>
         <ListPlaces></ListPlaces>
-    
-        
       </div>
     </div>
   );
